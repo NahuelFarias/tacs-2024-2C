b0b5d52765d17d735809b065af3c4d7cb7dd23c5
Nahuel Farias <nahuelfarias812@gmail.com>
2024-09-18 21:13:40 -0300

    Add: connection to the backend with axios

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Menu from './components/Menu';
 import SearchBar from './components/SearchBar';
 import EventCard from './components/EventCard';
 import Footer from './components/Footer';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [eventos, setEventos] = useState([]);  // Estado para los eventos
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/users') //TODO: Sacar a var de entorno
+        .then(({data}) => {
+          console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA", data)
+          setEventos(data);  // Actualiza el estado con los eventos recibidos
+        })
+        .catch(error => console.error('Error fetching events:', error));
+  }, []);
+
 
   return (
     <div className="d-flex flex-column min-vh-100">
@@ -29,10 +41,10 @@ function App() {
       <div className="events-section container">
         <h2 className="text-white">Eventos Destacados</h2>
         <div className="row">
-          {/* Aquí agregarías un mapeo de tus eventos */}
-          <EventCard title="Evento 1" />
-          <EventCard title="Evento 2" />
-          <EventCard title="Evento 3" />
+           {/* Mapeo de los eventos obtenidos desde el backend */}
+           {eventos.map(evento => (
+            <EventCard key={evento.name} title={evento.name} />
+          ))}
         </div>
       </div>
 
