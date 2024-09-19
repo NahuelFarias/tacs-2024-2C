4d6b0ee521a86b3e5ea7d3032dbed6aa3efee0d6
Nahuel Farias <nahuelfarias812@gmail.com>
2024-09-19 01:22:43 -0300

    Add: new styles

--- front/src/components/SearchBar.js
+++ front/src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBar = () => (
   <div className="search-bar p-3">
-    <input type="text" className="form-control" placeholder="Buscar eventos o artistas..." />
+    <input type="text" className="form-control" placeholder="Buscar eventos o artistas..." style={{ maxWidth: "600px", margin: "0 auto" }}/>
   </div>
 );
 
