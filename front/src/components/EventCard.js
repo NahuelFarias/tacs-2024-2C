b2f74026d66fc1251aaa34569e5ddedbc7519813
Nahuel Farias <nahuelfarias812@gmail.com>
2024-09-18 21:09:46 -0300

    Add: img in EventCard

--- front/src/components/EventCard.js
+++ front/src/components/EventCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './EventCard.css';
 
 const EventCard = ({ title }) => (
   <div className="col-md-4 mb-4">
     <div className="card bg-secondary text-white">
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
-        <p className="card-text">Aquí va una imagen</p>
+        <img src="/evento.jpg" alt="Icono" className="evento" />
       </div>
     </div>
   </div>
