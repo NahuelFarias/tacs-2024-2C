8447062d12257be8473fdcd2f83a568bfd3d56a6
ucabaleiro <ulises.cabaleiro@despegar.com>
2024-09-21 23:37:17 -0300

    add routing, move home to separate component

--- front/src/App.js
+++ front/src/App.js
@@ -1,55 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Menu from './components/Menu';
-import SearchBar from './components/SearchBar';
-import EventCard from './components/EventCard';
 import Footer from './components/Footer';
-import { getEvents } from './services/eventService';
+import Registration from './components/Registration';
+import Home from './components/Home';
+import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [eventos, setEventos] = useState([]);  // Estado para los eventos
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
-
-  useEffect(() => {
-    getEvents()
-      .then(data => {
-        setEventos(data);
-      })
-      .catch(error => console.error('Error fetching events:', error));
-  }, []);
-
-
+  
   return (
+    <Router>
     <div className="d-flex flex-column min-vh-100">
-      {/* Header */}
+    
       <Header toggleMenu={toggleMenu} />
 
-      {/* Menu */}
       <Menu menuOpen={menuOpen} />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Registration/>}/>
+        {/* <Route path='/event/:id' element={<Home eventos={eventos}/>}/> */}
+      </Routes>
 
-      {/* Search bar */}
-      <SearchBar />
-
-      {/* Eventos */}
-      <div className="events-section container">
-        <h2 className="text-white">Eventos Destacados</h2>
-        <div className="row">
-           {/* Mapeo de los eventos obtenidos desde el backend */}
-           {eventos.map(evento => (
-            <EventCard key={evento.id} title={evento.nombre} />
-          ))}
-        </div>
-      </div>
-
-      {/* Footer */}
       <Footer />
     </div>
+    </Router>
+    
   );
 }
 
