bacefa19fca8642b06507e90973d9e4cf2f31a8d
Nahuel Farias <nahuelfarias812@gmail.com>
2024-09-22 17:53:18 -0300

    Add: new event method service

--- front/src/services/eventService.js
+++ front/src/services/eventService.js
@@ -8,3 +8,16 @@ export const getEvents = () => {
       throw error;
     });
 };
+
+export const getEventByName = (eventName) => {
+  return axiosClient.get('/events/search', {
+      params: {
+        name: eventName
+      }
+    })
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error al obtener eventos:', error);
+      throw error;
+    });
+};
