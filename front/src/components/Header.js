e092ed441608273776d7bac4da02af59ecb6d46f
Fmozzi <francisco.mozzi.r@gmail.com>
2024-09-23 21:15:49 -0300

    add navigation for home to header logo

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import {React, useEffect, useRef} from 'react';
+import  { useNavigate } from 'react-router-dom';
 import './Header.css';
 
-const Header = ({ toggleMenu }) => (
+const Header = ({ toggleMenu }) => {
+
+  const navigate = useNavigate();
+
+  const redirectToHome = () => {
+    navigate(`/`);
+  };
+  
+  let logoRef = useRef();
+  useEffect(() => {
+    let closeReservationModal = (e) => {
+        if(logoRef.current.contains(e.target)){
+          redirectToHome();
+        }
+    };
+  
+    document.addEventListener("mousedown", closeReservationModal);
+  
+    return() =>{
+        document.removeEventListener("mousedown", closeReservationModal);
+    }
+  });
+
+  return(
   <header className="bg-dark text-white p-3 d-flex justify-content-between align-items-center">
-    <div className="logo">
+    <div className="logo"  ref={logoRef}>
         <img src="/icono.png" alt="Icono" className="logo-icon" /> {/* Usamos la imagen */}
         <h1 className="text-white ml-3">Events</h1>
     </div>
@@ -11,6 +35,7 @@ const Header = ({ toggleMenu }) => (
       <i className="fa fa-bars"></i> {/* Ícono de menú */}
     </button>
   </header>
-);
+  );
+};
 
 export default Header;
