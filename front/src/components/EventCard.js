a11a245b04a0dc6f12af93fdf297171dd11a362b
Fmozzi <francisco.mozzi.r@gmail.com>
2024-09-24 15:36:49 -0300

    remove event Id from eventCard title

--- front/src/components/EventCard.js
+++ front/src/components/EventCard.js
@@ -29,7 +29,7 @@ const EventCard = ({ eventId, title }) => {
   <div className="col-md-4 mb-4" ref={eventCardRef}>
     <div className="card bg-secondary text-white">
       <div className="card-body">
-        <h5 className="card-title">{title} - {eventId}</h5>
+        <h5 className="card-title">{title}</h5>
         <img src="/evento.jpg" alt="Icono" className="evento" />
       </div>
     </div>
