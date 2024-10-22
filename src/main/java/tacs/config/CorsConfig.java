893f6208f9e21b9359551b7eed051f1b0c6f5256
Nahuel Farias <nahuelfarias812@gmail.com>
2024-10-22 16:53:55 -0300

    Fix: CORS on cloud

--- src/main/java/tacs/config/CorsConfig.java
+++ src/main/java/tacs/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://"+ frontDomainDeploy + ":" + frontPort)
+                        .allowedOrigins("http://"+ frontDomainDeploy)
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
