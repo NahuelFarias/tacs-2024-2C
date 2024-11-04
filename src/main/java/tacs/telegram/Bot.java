d838224ab30086d576a08d0a27d0580bf19035ab
tomasneznajko <82177969+tomasneznajko@users.noreply.github.com>
2024-11-03 21:12:28 -0300

    Todavía no me funca :c

---
+++ src/main/java/tacs/telegram/Bot.java
@@ -0,0 +1,47 @@
+package tacs.telegram;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("Recibí tu mensaje: " + messageText);
+
+            try {
+                execute(message); // Enviar mensaje de respuesta
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Value("${telegram.bot.username}")
+    private String botUsername;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+}
