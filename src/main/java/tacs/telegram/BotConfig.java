96da0bfb7449154490cbce9ed435352d536a5b02
tomasneznajko <82177969+tomasneznajko@users.noreply.github.com>
2024-11-05 22:58:24 -0300

    Bueno, ahora anda, hay que agregar todas las funcionalidades que damos en el front

---
+++ src/main/java/tacs/telegram/BotConfig.java
@@ -0,0 +1,18 @@
+package tacs.telegram;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+//Registramo el bot en Spring Boot
+@Configuration
+public class BotConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(Bot bot) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(bot);
+        return botsApi;
+    }
+}
