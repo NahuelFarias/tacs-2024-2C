8626a837e767679ba322ab61177ec62ecb4e2184
Nahuel Farias <nahuelfarias812@gmail.com>
2024-11-09 21:07:03 -0300

    Add: config to use bot with ngrok

--- src/main/java/tacs/telegram/Bot.java
+++ src/main/java/tacs/telegram/Bot.java
@@ -2,37 +2,22 @@ package tacs.telegram;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
-public class Bot extends TelegramLongPollingBot {
-    @Override
-    public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
-
-
-            SendMessage message = new SendMessage();
-            message.setChatId(chatId);
-            message.setText("Recibí tu mensaje: " + messageText);
-
-            try {
-                execute(message); // Enviar mensaje de respuesta
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+public class Bot extends TelegramWebhookBot {
 
     @Value("${telegram.bot.token}")
     private String botToken;
 
     @Value("${telegram.bot.username}")
     private String botUsername;
+
+    @Value("${telegram.bot.path}")
+    private String botPath;
 
     @Override
     public String getBotUsername() {
@@ -44,4 +29,24 @@ public class Bot extends TelegramLongPollingBot {
         return botToken;
     }
 
+    @Override
+    public String getBotPath() {
+        return botPath;
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("Recibí tu mensaje: " + messageText);
+
+            return message;
+        }
+
+        return null;
+    }
 }
