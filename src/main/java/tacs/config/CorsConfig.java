d3e981eead5797bad259204494a0600ca96ea338
ayaxsantos <ayax.asantos@gmail.com>
2024-11-11 00:15:56 -0300

    Add: docker compose with building

--- src/main/java/tacs/config/CorsConfig.java
+++ src/main/java/tacs/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://tacs-frontend", "http://localhost")
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -22,4 +22,3 @@ public class CorsConfig {
         };
     }
 }
-
