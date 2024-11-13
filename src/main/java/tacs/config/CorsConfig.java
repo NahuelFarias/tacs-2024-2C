a875885419a8fdac75ab43514da92aef974c7356
Fmozzi <francisco.mozzi.r@gmail.com>
2024-11-12 21:00:03 -0300

    revert config

--- src/main/java/tacs/config/CorsConfig.java
+++ src/main/java/tacs/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://"+ frontDomainDeploy, "http://localhost:8080","http://localhost:3000")
+                        .allowedOrigins("http://"+ frontDomainDeploy)
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
