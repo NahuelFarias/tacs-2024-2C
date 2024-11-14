1321a33af2b9026a59db72fda9f8aad6e61267c4
ucabaleiro <ulises.cabaleiro@despegar.com>
2024-11-13 23:24:41 -0300

    allow PUT on CorsConfig

--- src/main/java/tacs/config/CorsConfig.java
+++ src/main/java/tacs/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://"+ frontDomainDeploy)
-                        .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
