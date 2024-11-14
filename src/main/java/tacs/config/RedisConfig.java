71eeca49924d104f5c0cee09e1698acf450034c4
ucabaleiro <ulises.cabaleiro@despegar.com>
2024-11-14 13:53:09 -0300

    fix redis host (bis)

--- src/main/java/tacs/config/RedisConfig.java
+++ src/main/java/tacs/config/RedisConfig.java
@@ -2,10 +2,12 @@ package tacs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@PropertySource("application.properties")
 @Configuration
 public class RedisConfig {
 
